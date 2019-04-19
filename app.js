44fc4f63fc7144bf260e064bba3ddacd54e4b4d5
Suraj Chand <surajchan68@gmail.com>
2019-04-19 14:50:50 +0545

    Adding User and Auth modules

--- app.js
+++ app.js
@@ -4,4 +4,10 @@ const parser = require('body-parser');
 
 app.use(parser.json());
 
+const dbConnector = require('./lib/helpers/database.helper');
+dbConnector.init(app);
+
+const router = require('./lib/route/index');
+router.init(app);
+
 module.exports = app;
