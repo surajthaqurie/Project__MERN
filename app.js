9435250cf1567223ce9b3f1d7d69177900eaeb7a
Suraj Chand <surajchan68@gmail.com>
2019-04-21 09:23:56 +0545

    error handling middleware and error logger through winston

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const parser = require('body-parser');
 const passport = require('passport');
+const logWriter = require('./lib/helpers/logwriter.helper');
+const errorHandler = require('./lib/middleware/error.handler');
+
+
+
 app.use(parser.json());
+app.use(cors());
+// app.use(express.static('./public'));
 
 const dbConnector = require('./lib/helpers/database.helper');
 dbConnector.init(app);
@@ -13,5 +21,9 @@ router.init(app);
 app.use(passport.initialize());
 app.use(passport.session());
 require('./lib/modules/auth/method/passport')(passport);
+
+logWriter.init(app);
+
+app.use(errorHandler);
 
 module.exports = app;
